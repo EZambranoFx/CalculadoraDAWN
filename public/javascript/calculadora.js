2cc41374c729ef28a59b9c5dfbbd2c4ae219bda0
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-06-11 18:40:36 -0500

    Add files via upload

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -3,15 +3,36 @@ let funcionPredeterminada=()=>{
     const formulario = document.getElementById('formulario')
     formulario.addEventListener('submit', (evento) => {
         evento.preventDefault();
-        let valorPorHora=parseFloat(document.getElementById('valorPorHora').value)
-        let interfazHoras=parseFloat(document.getElementById('interfazHoras').value)
-        let testingHoras=parseFloat(document.getElementById('testingHoras').value)
-        let HTMLHoras=parseFloat(document.getElementById('HTMLHoras').value)/*new*/
-        let CSSHoras=parseFloat(document.getElementById('CSSHoras').value)/*new*/
-        let JavaScriptsHoras=parseFloat(document.getElementById('JavaScriptsHoras').value)/*new*/
-        let APIHoras=parseFloat(document.getElementById('APIHoras').value)/*new*/
+        let valorPorHora=0, interfazHoras=0, testingHoras=0, HTMLHoras=0, CSSHoras=0, JavaScriptsHoras=0, APIHoras=0
+        if(parseFloat(document.getElementById('valorPorHora').value)>0){
+            valorPorHora=parseFloat(document.getElementById('valorPorHora').value)
+        }
+        
+        if(parseFloat(document.getElementById('interfazHoras').value)>0){
+            interfazHoras=parseFloat(document.getElementById('interfazHoras').value)
+        }
 
-        let resultado=(valorPorHora*interfazHoras)+(valorPorHora*testingHoras)+(valorPorHora*HTMLHoras)+(valorPorHora*CSSHoras)+(valorPorHora*JavaScriptsHoras)+(valorPorHora*APIHoras)
+        if(parseFloat(document.getElementById('testingHoras').value)>0){
+            testingHoras=parseFloat(document.getElementById('testingHoras').value)
+        }
+        
+        if(parseFloat(document.getElementById('HTMLHoras').value)>0){
+            HTMLHoras=parseFloat(document.getElementById('HTMLHoras').value)
+        }
+
+        if(parseFloat(document.getElementById('CSSHoras').value)>0){
+            CSSHoras=parseFloat(document.getElementById('CSSHoras').value)
+        }
+
+        if(parseFloat(document.getElementById('JavaScriptsHoras').value)>0){
+            JavaScriptsHoras=parseFloat(document.getElementById('JavaScriptsHoras').value)
+        }
+
+        if(parseFloat(document.getElementById('APIHoras').value)>0){
+            APIHoras=parseFloat(document.getElementById('APIHoras').value)
+        }
+
+        resultado=(valorPorHora*interfazHoras)+(valorPorHora*testingHoras)+(valorPorHora*HTMLHoras)+(valorPorHora*CSSHoras)+(valorPorHora*JavaScriptsHoras)+(valorPorHora*APIHoras)
 
         document.getElementById('valorTotal').value=resultado
     })
