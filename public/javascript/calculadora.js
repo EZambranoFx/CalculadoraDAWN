0c22ac3d120af6695470b22c786af6d19f7899e0
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-06-11 18:09:16 -0500

    Update calculadora.js

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -6,7 +6,12 @@ let funcionPredeterminada=()=>{
         let valorPorHora=parseFloat(document.getElementById('valorPorHora').value)
         let interfazHoras=parseFloat(document.getElementById('interfazHoras').value)
         let testingHoras=parseFloat(document.getElementById('testingHoras').value)
-        let resultado=(valorPorHora*interfazHoras)+(valorPorHora*testingHoras)
+        let HTMLHoras=parseFloat(document.getElementById('HTMLHoras').value)/*new*/
+        let CSSHoras=parseFloat(document.getElementById('CSSHoras').value)/*new*/
+        let JavaScriptsHoras=parseFloat(document.getElementById('JavaScriptsHoras').value)/*new*/
+        let APIHoras=parseFloat(document.getElementById('APIHoras').value)/*new*/
+
+        let resultado=(valorPorHora*interfazHoras)+(valorPorHora*testingHoras)+(valorPorHora*HTMLHoras)+(valorPorHora*CSSHoras)+(valorPorHora*JavaScriptsHoras)+(valorPorHora*APIHoras)
 
         document.getElementById('valorTotal').value=resultado
     })
